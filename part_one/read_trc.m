b5f16b5fa17030b3c15fffc7d6ee5b8667a568a7
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-11-10 12:28:28 -0500

    Update read_trc.m

--- part_one/read_trc.m
+++ part_one/read_trc.m
@@ -7,9 +7,7 @@ function data = read_trc(file)
 % supply full path to .trc file
 % returns table of marker data
 
-% Read the data from the .trc file
-data = readtable(file, "FileType", "Text", "VariableNamingRule", "Preserve", "VariableNamesLine", 4, "NumHeaderLines", 6);
-% only keep the first 182 columns
+data = readtable(file, "FileType", "Text", "VariableNamesLine", 4, "NumHeaderLines", 6, 'PreserveVariableNames', 1);
 data = data(:,1:182);
 end
 
